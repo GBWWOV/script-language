9a671416e4de47b7791bf5f347cd60f8a9dc51f1
GBWWOV <sebestyen.peter00@gmail.com>
2021-10-08 15:15:52 +0200

    2. ora

--- 2/password_gen.py
+++ 2/password_gen.py
@@ -1,18 +1,18 @@
+import string
+import random
 
 
-length = 4
-password = ""
+def get_random_pwd(length, use_digit, use_punctuation):
+    available_characters = string.ascii_letters
+    if use_digit:
+        available_characters = available_characters + string.digits
+    if use_punctuation:
+        available_characters = available_characters + string.punctuation
 
-use_digit= True
-
-available_characters = string.ascii_letters
-if use_digit:
-    available_characters = available_characters + string.digits
-items_count = len(available_characters)
-
-for _ in range(length):
-    index = random.randint(0, items_count - 1)
-    password = password + available_characters[index]
+    password = "".join(random.choice(available_characters) for _ in range(1, length))
+    return password
 
 
-print(f"generated password: {password}")
+if __name__ == '__main__':
+
+    print(f"generated password: {get_random_pwd(length = 9, use_digit = True, use_punctuation = True)}")
